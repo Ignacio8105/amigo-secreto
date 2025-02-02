bd18f2bc49c3721e142ba0f1437a811b6a370400
Ignacio <mora00_81@hotmail.com>
2025-02-01 22:55:05 -0600

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables
+let amigos=[];
+let campoEntrada=""
+let validarEntrada=""
+
+//funcion 
+function asignarNombre(elemento, texto){
+    let elementoHTML= document.querySelector(elemento);
+    elementoHTML.innerHTML= texto;
+    return;
+}
+
